08d2c5dfc5667689b9f02cd39573c8de0f593505
suryagit3 <suryagit3>
2021-10-06 16:51:52 +0530

    homepage commit update

--- pom/batpom/flask_bat.py
+++ pom/batpom/flask_bat.py
@@ -1,13 +1,19 @@
 from flask import *
-
+import requests
 app = Flask(__name__)
 app.secret_key = "asdfa"
-
+host = "http://localhost:5000/"
 @app.route("/",methods=['POST','GET'])
 def index():
     tree_list = [["test_py_parent",["test_children1","test_children2","test_children3"]],["test_py_parent2",["test_children1","test_children2","test_children3"]]]
     # issues = []
-    return render_template("homepage.html",tree_lists=tree_list)
+    return render_template("homepage.html",tree_lists=tree_list,host_name=host+"run")
+@app.route("/run",methods=['POST'])
+def run():
+    if request.method == "POST":
+        return request.form
+    else:
+        return 400,"Method not allowed"
 
 if __name__ == "__main__":
     app.run()
